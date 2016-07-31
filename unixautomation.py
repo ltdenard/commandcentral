217de0738baa882cc6c40b36c0a12f3f348b2bef
Leon Denard <ltdenard@gmail.com>
2016-07-31 04:09:01 -0400

    first attempt at trying to get base re-written properly into classes instead of jamming a bunch of functions into one file

--- unixautomation.py
+++ unixautomation.py
@@ -24,25 +24,36 @@ class UnixAutomation:
             serverlist = f.read().splitlines()
         return serverlist
 
+    def writeoutput(self, server, array):
+        if self._outputfile is not None:
+            with open(self._outputfile, "a") as f:
+                f.write("%s,%s\n" % (server, array))
+        return
+
+    def setuplog(self, logpreface=None, sessionobj=None):
+        if logpreface is not None:
+            filedate = datetime.datetime.today().strftime('%Y-%m-%d-%s')
+            filename = logpreface + '_' + filedate + ".csv"
+            sessionobj._logfile = sessionobj._logfolder + logpreface + '.log'
+            sessionobj._outputfile = sessionobj._logfolder + filename
+            paramiko.util.log_to_file(self._logfile)
+        return
+
     def login(self, server):
-        runlog = self._logfile
-        paramiko.util.log_to_file(runlog)
+        self.setuplog()
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(server, username=self._username, password=self._password, pkey=self._privatekey)
             return ssh
         except (paramiko.ssh_exception.SSHException) as e:
-            with open(self._outputfile, "a") as f:
-                f.write("%s,%s\n" % (server, [str(e)]))
+            self.writeoutput(server, [str(e)])
             return
         except socket.error as e:
-            with open(self._outputfile, "a") as f:
-                f.write("%s,%s\n" % (server, [str(e)]))
+            self.writeoutput(server, [str(e)])
             return
         except Exception as e:
-            with open(self._outputfile, "a") as f:
-                f.write("%s,%s\n" % (server, [str(e)]))
+            self.writeoutput(server, [str(e)])
             return
     
     def logintest(self, sessionobj):
@@ -56,12 +67,29 @@ class UnixAutomation:
 
     def runcommand(self, sessionobj, execmd):
         ssh_stdin, ssh_stdout, ssh_stderr = sessionobj.exec_command(execmd)
-        return ssh_stdin, ssh_stdout, ssh_stderr
+        return_array = [re.sub(' +', ' ', x.strip()) for x in ssh_stdout.read().splitlines()]
+        exit_code = 0
+        if ssh_stdout.channel.recv_exit_status() != 0:
+            exit_code = 1
+        return exit_code,return_array
+
+    def getuname(self, sessionobj):
+        exit_code,uname = self.runcommand(sessionobj,"uname")
+        return exit_code,uname
+
+    def getrelease(self, sessionobj):
+        exit_code,release = self.runcommand(sessionobj,"cat /etc/*release")
+        return exit_code,release
 
     def gethostname(self, sessionobj):
+        exit_code,hostname = self.runcommand(sessionobj,"hostname")
+        return exit_code,hostname
+    
+    def threadcommand(self, sessionobj, tclass):
         queue = Queue.Queue()
+        threadclass = tclass
         for i in range(30):
-            t = GetHostname(queue, sessionobj)
+            t = threadclass(queue, sessionobj)
             t.setDaemon(True)
             t.start()
         for server in self.serverlist():
@@ -79,23 +107,54 @@ class GetHostname(threading.Thread, UnixAutomation):
     def run(self):
         while True:
             server = self._queue.get()
-            self.gethostname(self._sessionobj, server)
+            self.hostname(self._sessionobj, server)
             self._queue.task_done()
 
-    def gethostname(self, sessionobj, server):
-        filedate = datetime.datetime.today().strftime('%Y-%m-%d-%s')
-        filename = "hostname_pull_" + filedate + ".csv"
-        self._sessionobj._logfile = self._sessionobj._logfolder + 'gethostname.log'  
-        self._sessionobj._outputfile = self._sessionobj._logfolder + filename 
+    def hostname(self, sessionobj, server):
+        self._sessionobj.setuplog('hostname_pull',sessionobj)
         ssh = self._sessionobj.login(server)
         if self._sessionobj.logintest(ssh) is 0:
-            ssh_stdin, ssh_stdout, ssh_stderr = self._sessionobj.runcommand(sessionobj=ssh,execmd="hostname")
-            hostname = [re.sub(' +', ' ', x.strip()) for x in ssh_stdout.read().splitlines()]
-            self._sessionobj.logout(ssh)
-            with open(sessionobj._outputfile, "a") as f:
-                f.write("%s,%s\n" % (server, hostname))
+            exit_code,hostname = self._sessionobj.gethostname(ssh)
+            if exit_code != 0: 
+                self._sessionobj.writeoutput(server, 'command execution failed')
+                return
+            self._sessionobj.writeoutput(server, hostname)
             self._sessionobj.logout(ssh)            
-            return
-        else:
-            return
+        return
 
+
+class ChangeRootPassword(threading.Thread, UnixAutomation):
+
+    def __init__(self, queue, sessionobj):
+        threading.Thread.__init__(self)
+        self._queue = queue
+        self._sessionobj = sessionobj
+
+    def run(self):
+        while True:
+            server = self._queue.get()
+            self.changeroot(self._sessionobj, server)
+            self._queue.task_done()
+
+    #def changeaix():
+    #def changelinux():
+    #def changesolaris():
+
+    def changeroot(self, sessionobj, server):
+        self._sessionobj.setuplog('change_root',sessionobj)
+        ssh = self._sessionobj.login(server)
+        if self._sessionobj.logintest(ssh) is 0:
+            exit_code,uname = self._sessionobj.getuname(ssh)
+            if exit_code != 0: 
+                self._sessionobj.writeoutput(server, 'command execution failed')
+                self._sessionobj.logout(ssh)
+                return
+            if uname[0] == 'Linux':
+                changelinux(ssh)
+            elif uname[0] == 'AIX':
+                changeaix(ssh)
+            elif uname[0] == 'SunOS':
+                changesolaris(ssh)
+            # self._sessionobj.writeoutput(server, hostname)
+            self._sessionobj.logout(ssh)            
+        return
