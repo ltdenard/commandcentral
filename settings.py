4d1bdad744b42401a0e0b68afd48cab15701eade
ltdenard <ltdenard@gmail.com>
2016-08-15 23:25:13 -0400

    adding ssh key management classes and functions

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 fortyfiveday_expire_servers = [
-	# Example: 'hostname',
+    # Example: 'hostname',
 ]
 authorized_ssh_keys = [
-	# Example: 'hostname : ssh-rsa ...',
+    # Example: 'hostname : ssh-rsa ...',
+    'x1 : ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDiNR3JJxEA4LGM1PZE98boKgOQQtEWfuthpmGpZncTYTKL789MMyt9M2xghqe6AMyRjyGH5TXk5saSKdvoS5r0K7qqLvjquSLSSkKR0ZavR29A9uWD0tmW55Aw4jUSann9+vqCndfC1AzAmXdV/KulVvCRH9MnILSQUsOdw6XwbgTD51oyEYKFQVAqt7PHMqTlK7sAQ12PlN3ua7HDxGKWslBfctlIWU2NxE/2EWkL/LNDCdnoJ6MtYeSaF460nU8CXyXjRKKtmI79VdKkkl8R5+D4uMHBI6ZqjyjC9pbsjFJawQMM/vmbUAfzHu8z0yNBoaep7XkYdKMxykSXxlafFGwvKm7gjnzROX+hGP9DwSJz9fSzTSByoMwzqC2rOM9A8GRAaTU29WsdHr1j3p3rREDBZlOHiR/sBNcsbbiJ0hCIWi7jX/bedPzo3Lbzpzx6+rZqtpU+JNy72WAsXSSj3ibu1LRIW3tj8XpAr1ByhWjeZu6RguZgcfYdsauKRGVdGkL8QhOMixeyaCIMoJ5L7S47Y1VdhAq40pSp7sSy+oQ7eoWx2P9/gYTQ4CNuUZiVmfDYVxpnbtPrLoQwcMRW4HB0iZOJZivLahgUHpT3e8HRo7g6m5Nn3z2ntKkN1YDV9ZbiTPs4MJJkTjIiRv6gIAzycRvxGWsPQ5T9Iv/MSQ== denardl@x1'
 ]
 global_authorized_ssh_keys = [
-	# Example: 'ssh-rsa ...',
+    # Example: 'ssh-rsa ...',
+    'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDM0G/nz5Ue8sRvZsRkskqBGpmqVsx6GFcihWhzxklBa8+BOfa206STLRTXibO2UJJtDIRhj5mxukliH5ezA+PWNDWWwA94L4S70twdXNJQ/ZVMpD8RhyfVKemJagarmR5u/xHK74slSvNdKqDYy4HXJetjfBncq/WMENIf6+g+9i3FBuRn8FC+SVBg5QMSQl4fI+EF4QuDISLuh87PsVUDZpBnKLdrojnYFlrt5ln17ZBwnYim6DIJsix2qrTJXpW4bh47nqIs/gsX1B7wPiAOqLNLavFvrna3yNf+vw8H0FzZYMtGC5Gq8Qwt1N9rHCjt5YmoD4rJC32Xjv68bmv+c3G3q8uhNQWhuguPD1ErOKtOigduFR9qX4dm0h6Bt3A9C+1GWIIDWos9NkoqB8nizuUu+k1iNwF9Py4cCoA/m3TklFduXRCL9eUBJRiNcJGXaBmzoSXBPOiT3Ywphmx5jmrL6PgVzLwoetW8deUgLV+4lHBVxniMfFJ74pOpQjjSlPvHTKoQEIGZPUWOgG0jIpit1FcREmObiPhNh21m9vNM+FfyORzX4OZJEZ4PibocHqGVpD9myw8IhHRR0W226GRH8T1nseuwiPwBorj4p8bnoqi5nHQiuI3dRBGNZEHiY/YCmZveARMxjs4Z/alD8s6qHBVTKOs6RCHadirvWw== denardl@denardl-mbp'
 ]
